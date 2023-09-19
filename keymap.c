c85582f778bceb5de3b90b154f52419c79e8cb00
drevilbones <drevilbones@gmail.com>
2023-09-19 15:45:05 -0400

    added layer for mouse keys on numpad

--- keymap.c
+++ keymap.c
@@ -21,7 +21,8 @@ enum planck_layers {
   _FRN,
   _ADJ,
   _KBD,
-  _MUS
+  _MUS,
+  _MOU
 };
 
 enum planck_keycodes {
@@ -42,25 +43,30 @@ enum planck_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_FRN] = LAYOUT_ortho_4x12( //left side configured for gaming, right side is a num pad
-          KC_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LOCK,  KC_NLCK,  KC_P7,    KC_P8,    KC_P9,    KC_PSLS,    
+          QK_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    QK_LOCK,  KC_NUM,   KC_P7,    KC_P8,    KC_P9,    KC_PSLS,    
           KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_I,     KC_BSPC,  KC_P4,    KC_P5,    KC_P6,    KC_PAST,    
           KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_K,     MO(_MUS), KC_P1,    KC_P2,    KC_P3,    KC_PMNS,    
-          KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_SPC,  KC_LALT, KC_M,     TT(_ADJ), KC_P0,    KC_PDOT,  KC_ENT,   KC_PPLS),
+          KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_SPC,  KC_LALT, KC_M,     MO(_ADJ), KC_P0,    KC_PDOT,  KC_ENT,   KC_PPLS),
         [_ADJ] = LAYOUT_ortho_4x12( //extended keys
           KC_TRNS,  KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_NO,    KC_CALC,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
           KC_TRNS,  KC_TRNS,  KC_UP,    KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,    KC_NO,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, 
           KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,KC_TRNS, KC_TRNS, KC_NO,    MO(_KBD), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, 
-          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PENT,  KC_TRNS),
+          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PENT,  TG(_MOU)),
         [_KBD] = LAYOUT_ortho_4x12( //Internal keyboard functions
-          RESET,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+          QK_BOOT,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
           KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
           KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,
           KC_NO,    KC_NO,    RGB_TOG,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO),          
         [_MUS] = LAYOUT_ortho_4x12( //dumb sfx to annoy everybody in the room
-          AU_TOG,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    PLSO7,    PLSO8,    PLSO9,    KC_NO,
+          AU_TOGG,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    PLSO7,    PLSO8,    PLSO9,    KC_NO,
           KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    PLSO4,    PLSO5,    PLSO6,    PLSOM,
           KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,  PLSO1,    PLSO2,    PLSO3,    PLSOS,
-          KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,  PLSO0,    PLSTO,    KC_NO,    KC_NO)
+          KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,  PLSO0,    PLSTO,    KC_NO,    KC_NO),
+        [_MOU] = LAYOUT_ortho_4x12( //mouse keys on the number pad
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_BTN1,  KC_MS_U,  KC_BTN2, KC_TRNS,
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MS_L,  KC_NO,    KC_MS_R, KC_WH_U,
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_BTN3,  KC_MS_D,  KC_BTN4, KC_WH_D,
+           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_WH_L,  KC_WH_R,  KC_TRNS, KC_TRNS)
 };
 
         // [_TMP] = LAYOUT_ortho_4x12( //template
